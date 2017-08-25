f16ec04a2c6dd09ab9fecc2b21a72aa8e1a44a1f
njucjc <njucjc@gmail.com>
2017-08-25 19:23:40 +0800

    solved Max2Tests

--- src/test/scala/problems/Max2Tests.scala
+++ src/test/scala/problems/Max2Tests.scala
@@ -11,11 +11,12 @@ class Max2Tests(c: Max2) extends PeekPokeTester(c) {
   for (i <- 0 until 10) {
 
     // Implement below ----------
-
-    poke(c.io.in0, 0)
-    poke(c.io.in1, 0)
+	val in0 = rnd.nextInt(256)
+	val in1 = rnd.nextInt(256)
+    poke(c.io.in0, in0)
+    poke(c.io.in1, in1)
     step(1)
-    expect(c.io.out, 1)
+    expect(c.io.out, if(in0 > in1) in0 else in1)
 
     // Implement above ----------
   }
