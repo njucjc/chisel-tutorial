0b9bcfa175aa36a4eef9b71b1e683d8fa364f85d
njucjc <njucjc@gmail.com>
2017-09-01 01:53:55 +0800

    solved VendingMachine

--- src/main/scala/problems/VendingMachine.scala
+++ src/main/scala/problems/VendingMachine.scala
@@ -21,11 +21,29 @@ class VendingMachine extends Module {
   val sIdle :: s5 :: s10 :: s15 :: sOk :: Nil = Enum(5)
   val state = RegInit(sIdle)
 
-  // Implement below ----------
+  when (state === sIdle) {
+    when (io.nickel) { state := s5 }
+    when (io.dime)   { state := s10 }
+  }
 
-  state := s5
+  when (state === s5) {
+    when (io.nickel) { state := s10 }
+	when (io.dime)   { state := s15 }
+  }
 
-  // Implement above ----------
+  when (state === s10) {
+    when (io.nickel) { state := s15 }
+	when (io.dime)   { state := sOk }
+  }
+
+  when (state === s15) {
+    when (io.nickel) { state := sOk }
+	when (io.dime)   { state := sOk }
+  }
+
+  when (state === sOk) {
+    state := sIdle
+  }
 
   io.valid := (state === sOk)
 }
